d96530f9a53b24a62e334489d2524d9616815711
ryuuu kun <134138793+ryuuu-kun@users.noreply.github.com>
2023-06-14 01:07:35 +0700

    Update Footer.vue

--- src/components/home/Footer.vue
+++ src/components/home/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="mt-8 flex justify-center items-center dark:text-white">
+      <p>Muhammad Sigit and Team</p> 
     <p>© 2023. All rights reserved</p>
   </div>
 </template>
